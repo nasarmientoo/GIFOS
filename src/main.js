b4b1679880353b4162d4a8be132e104377a6dc5b
nasarmientoo <nasarmientoo@correo.udistrital.edu.co>
2021-09-02 11:18:45 -0500

    Fetch para multiples APIs, no funcionando, mirar stack overflow

--- src/main.js
+++ src/main.js
@@ -1,19 +1,28 @@
-document.addEventListener('DOMContentLoaded', init);
-
-function init () {
-    document.getElementById('btnSearch').addEventListener('click', e =>{
-        e.preventDefault();
-        let apiKey = 'fWThAF0VpzbGsNMM8hag7y8u9OJjig7y';
-        let userInput = document.getElementById('search').value.trim()
-        let url = `https://api.giphy.com/v1/gifs/search?q=${userInput}&rating=g&api_key=${apiKey}&limit=20&offset=5`
-        
-        fetch(url)
-        .then(response => response.json())
-        .then(content => {
-            console.log('[+]',content.data)
-            console.log('[-]',content.meta)
-            
-        })
-        .catch(e => console.log(e))
-    })
+function getAllUrls(urls) {
+    return Promise.all(
+        urls.map(url => fetch(url)
+            .then(response => response.json())
+            .then(content => {
+                console.log('[+]', content.data)
+                console.log('[-]', content.meta)
+            })
+        )
+    )
 }
+
+function init() {
+    let apiKey = 'fWThAF0VpzbGsNMM8hag7y8u9OJjig7y';
+    let urls = [
+        `https://api.giphy.com/v1/gifs/trending?api_key=${apiKey}&limit=20&offset=3`,
+    ]
+
+    document.getElementById('btnSearch').onclick = function() {
+        let userInput = document.getElementById('search').value.trim()
+        let searchUrl = `https://api.giphy.com/v1/gifs/search?q=${userInput}api_key=${apiKey}&limit=20&offset=5`
+        return urls.push(searchUrl)
+    }
+
+    let responses = getAllUrls(urls)
+}
+
+init()
