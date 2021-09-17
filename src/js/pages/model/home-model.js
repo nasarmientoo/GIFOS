b81c58364017520dfeecc74c3ad21cc959a3c10c
nasarmientoo <nasarmientoo@correo.udistrital.edu.co>
2021-09-17 00:19:25 -0500

    Funcionalidad completa sección gifs buscados, sin estilo

--- src/js/pages/model/home-model.js
+++ src/js/pages/model/home-model.js
@@ -1,34 +1,50 @@
-//Renderizar tarjetas de gifs buscados
-function appendData(data, container, input) {
+//Sección de gifs buscados y funcionalidad al botón 'ver más'
+function appendData(input) {
+    let container = document.querySelector('#search-container')
     let content = ` 
                 <div class"gifs-container">
                     <h1 class="title">${input}</h1>
-                    <img id="show-more" src="assets/CTA-ver-mas.svg" type="button" onclick="loadData(${JSON.stringify(data).split('"').join("&quot;")})">
+                    <div id="section-cards"></div>
+                    <img id="show-more" src="assets/CTA-ver-mas.svg" type="button" onclick="loadMore()">
                 </div>`
     container.innerHTML = content
 }
 
+//Agregar toda la data de la API a la sección creada en Append Data
 window.loadData = (data) => {
-    /* for (let i = 0; i < data.length; i++) {
-        content += `
-                    <div class="section-cards">
-                        <img src="${data[i].images.downsized.url}">
-                    </div>
-                `
-    } */
-    console.log(data)
+    let cardSection = document.getElementById('section-cards')
+    let content = ''
+    for (let i = 0; i < data.length; i++) {
+        content += `<img class="gifs-images hidden"src="${data[i].images.downsized.url}">`
+    }
+    cardSection.innerHTML = content
 }
 
-//Acceder a la data buscada
+//Funcionalidad botón 'ver más'
+window.loadMore = () => {
+    let showMoreButton = document.getElementById('show-more')
+    let list = [...document.querySelectorAll('.gifs-images.hidden')]
+
+    list.splice(0, 3).forEach(
+        elem => elem.classList.remove('hidden')
+    );
+
+    if (list.length == 0) {
+        showMoreButton.classList.add('hidden');
+    }
+}
+
+//Acceder a la data buscada y renderizarla 
 export function getSearchData() {
-    let apiKey = 'fWThAF0VpzbGsNMM8hag7y8u9OJjig7y';
     let input = document.querySelector('#search').value.trim()
-    let container = document.querySelector('#search-container')
-    let searchUrl = `https://api.giphy.com/v1/gifs/search?q=${input}&api_key=${apiKey}&offset=12`
+    let apiKey = 'fWThAF0VpzbGsNMM8hag7y8u9OJjig7y';
+    let searchUrl = `https://api.giphy.com/v1/gifs/search?q=${input}&api_key=${apiKey}&limit=20&offset=12`
 
     fetch(searchUrl)
         .then(response => response.json())
         .then(content => {
-            appendData(content.data, container, input)
+            appendData(input)
+            loadData(content.data)
+            loadMore()
         })
 }
